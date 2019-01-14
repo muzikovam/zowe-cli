ee51492fc190e70620e285892c115c194940f854
zlapa01 <zlapa01@ca.com>
2019-01-14 15:42:54 +0100

    initial
    just dummy one, need clean up and update acording to properties + fix nmp issues

---
+++ packages/workflows/__tests__/api/PropertiesWorkflows.unit.test.ts
@@ -0,0 +1,125 @@
+    /*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { ZosmfRestClient } from "../../../rest";
+import { Session, ImperativeError, Imperative, Headers } from "@brightside/imperative";
+import { PropertiesWorkflows } from "../../../workflows";
+import { WorkflowConstants, noSession, noFilter } from "../../src/api/WorkflowConstants";
+//import { IProvisionedInstances, ListRegistryInstances, noSessionProvisioning, nozOSMFVersion, ProvisioningConstants } from "../../../provisioning";
+
+const category = "Provisioning";
+const system = "CA11";
+const statusName = "complete";
+const owner = "user";
+const vendor = "CA Technologies";
+
+let START_RESOURCE_QUERY: string = `${WorkflowConstants.RESOURCE}/${WorkflowConstants.ZOSMF_VERSION}/`;
+START_RESOURCE_QUERY += `${WorkflowConstants.WORKFLOW_RESOURCE}/${WorkflowConstants.PROPERTIES_WORKFLOWS}/?${category}/?${system}/?${statusName}/?${owner}/?${vendor}`;
+//START_RESOURCE_QUERY += `${ WorkflowConstants.RESOURCE }/${ WorkflowConstants.PROPERTIES_WORKFLOWS }`;
+
+const PRETEND_SESSION = new Session({
+    user: "usr",
+    password: "pasword",
+    hostname: "host.com",
+    port: 443,
+    type: "basic",
+    rejectUnauthorized: false
+});
+
+
+function expectZosmfResponseSucceeded(response: any, error: ImperativeError) {
+    expect(error).not.toBeDefined();
+    expect(response).toBeDefined();
+}
+
+function expectZosmfResponseFailed(response: any, error: ImperativeError, msg: string) {
+    expect(response).not.toBeDefined();
+    expect(error).toBeDefined();
+    expect(error.details.msg).toContain(msg);
+}
+
+
+
+describe("PropertiesWorkflows", () => {
+    //list all
+    it("Successfull call returns 200 - no message. Test properties all workflows.", async () => {
+        (ZosmfRestClient.putExpectString as any) = jest.fn<string>(() => {
+            return "";
+        });
+
+        let error: ImperativeError;
+        let response: any;
+        try {
+            response = await PropertiesWorkflows.propertiesWorkflows(PRETEND_SESSION);
+            Imperative.console.info(`Response ${response}`);
+        } catch (thrownError) {
+            error = thrownError;
+            Imperative.console.info(`Error ${error}`);
+        }
+        expect((ZosmfRestClient.putExpectString as any)).toHaveBeenCalledTimes(1);
+        expect((ZosmfRestClient.putExpectString as any)).toHaveBeenCalledWith(PRETEND_SESSION, START_RESOURCE_QUERY, [Headers.APPLICATION_JSON], { });
+        expectZosmfResponseSucceeded(response, error);
+        expect(response).toEqual("");
+    });   
+
+    //list with filters 
+    it("Successfull call returns 200 - no message. Test properties workflows with filter.", async () => {
+        (ZosmfRestClient.putExpectString as any) = jest.fn<string>(() => {
+            return "";
+        });
+
+        let error: ImperativeError;
+        let response: any;
+        try {
+            response = await PropertiesWorkflows.propertiesWorkflows(PRETEND_SESSION, category, owner, vendor, statusName, system);
+            Imperative.console.info(`Response ${response}`);
+        } catch (thrownError) {
+            error = thrownError;
+            Imperative.console.info(`Error ${error}`);
+        }
+    //    expect((ZosmfRestClient.putExpectString as any)).toHaveBeenCalledTimes(1);
+    //    expect((ZosmfRestClient.putExpectString as any)).toHaveBeenCalledWith(PRETEND_SESSION, START_RESOURCE_QUERY, [Headers.APPLICATION_JSON], { });
+    //    expectZosmfResponseSucceeded(response, error);
+   //     expect(response).toEqual("");
+    });  
+ 
+
+    //error session
+    it("should throw an error if the session parameter is undefined", async () => {
+        let error: ImperativeError;
+        let response: any;
+        try {
+            response = await PropertiesWorkflows.propertiesWorkflows(undefined,category);
+            Imperative.console.info(`Response ${response}`);
+        } catch (thrownError) {
+            error = thrownError;
+            Imperative.console.info(`Error ${error}`);
+        }
+        expectZosmfResponseFailed(response, error, noSession.message);
+    });
+
+    //empty filter
+    it("Should throw error if filter is empty string.", async () => {
+        let error: ImperativeError;
+        let response: any;
+        try {
+            response = await PropertiesWorkflows.propertiesWorkflows(PRETEND_SESSION, "");
+            Imperative.console.info(`Response ${response}`);
+        } catch (thrownError) {
+            error = thrownError;
+            Imperative.console.info(`Error ${error}`);
+        }
+        expectZosmfResponseFailed(response, error, noFilter.message);
+    });
+});
+
+
+
---
+++ packages/workflows/src/api/PropertiesWorkflows.ts
@@ -0,0 +1,57 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+
+import { AbstractSession, Headers } from "@brightside/imperative";
+import { ZosmfRestClient } from "../../../rest";
+import { WorkflowConstants, noFilter } from "./WorkflowConstants";
+import { WorkflowValidator } from "./WorkflowValidator";
+
+
+
+export class PropertiesWorkflows{
+    //Optional, request can include one or more parameters to filter the results
+    public static async propertiesWorkflows(session: AbstractSession, category?: string, system?: string, owner?: string, vendor?: string, statusName?: string) {
+        
+        const zOSMFVersion = WorkflowConstants.ZOSMF_VERSION;
+        WorkflowValidator.validateSession(session);
+        WorkflowValidator.validateNotEmptyString(category, noFilter.message);
+        WorkflowValidator.validateNotEmptyString(system, noFilter.message);
+        WorkflowValidator.validateNotEmptyString(owner, noFilter.message);
+        WorkflowValidator.validateNotEmptyString(vendor, noFilter.message);
+        WorkflowValidator.validateNotEmptyString(statusName, noFilter.message);
+           
+        let resourcesQuery: string = `${WorkflowConstants.RESOURCE}/${zOSMFVersion}/${WorkflowConstants.PROPERTIES_WORKFLOWS}`;
+
+        if (resourcesQuery += `${WorkflowConstants.category}=${category}`)
+        return ZosmfRestClient.getExpectJSON(session, resourcesQuery, [Headers.APPLICATION_JSON]);
+
+       // let resourcesQuery: string = `${WorkflowConstants.RESOURCE}/${zOSMFVersion}/${WorkflowConstants.LIST_WORKFLOWS}`;
+        if (resourcesQuery += `${WorkflowConstants.system}=${system}`)
+        return ZosmfRestClient.getExpectJSON(session, resourcesQuery, [Headers.APPLICATION_JSON]);
+
+       // let resourcesQuery: string = `${WorkflowConstants.RESOURCE}/${zOSMFVersion}/${WorkflowConstants.LIST_WORKFLOWS}`;
+       if (resourcesQuery += `${WorkflowConstants.owner}=${owner}`)
+       return ZosmfRestClient.getExpectJSON(session, resourcesQuery, [Headers.APPLICATION_JSON]);
+
+       // let resourcesQuery: string = `${WorkflowConstants.RESOURCE}/${zOSMFVersion}/${WorkflowConstants.LIST_WORKFLOWS}`;
+       if (resourcesQuery += `${WorkflowConstants.vendor}=${vendor}`)
+       return ZosmfRestClient.getExpectJSON(session, resourcesQuery, [Headers.APPLICATION_JSON]);
+
+       // let resourcesQuery: string = `${WorkflowConstants.RESOURCE}/${zOSMFVersion}/${WorkflowConstants.LIST_WORKFLOWS}`;
+       if (resourcesQuery += `${WorkflowConstants.statusName}=${statusName}`)
+       return ZosmfRestClient.getExpectJSON(session, resourcesQuery, [Headers.APPLICATION_JSON]);
+
+       return ZosmfRestClient.getExpectJSON(session, resourcesQuery, [Headers.APPLICATION_JSON]);
+      
+    }
+}
+
